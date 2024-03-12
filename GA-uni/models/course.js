85ce2766fbbb456df89b7ffabfab1bbb5d633da7
zahraa <zahraaalihasan279@gmail.com>
2024-03-12 15:44:08 +0300

    edit model schema

--- GA-uni/models/course.js
+++ GA-uni/models/course.js
@@ -4,6 +4,6 @@ const Schema = mongoose.Schema
 const courseSchema = new Schema({
   name: String,
   scores: [{ type: Schema.Types.ObjectId, ref: 'Score' }]
-  })
+})
 
-module.exports = studentSchema
+module.exports = courseSchema
