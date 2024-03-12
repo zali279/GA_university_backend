09251406dc585c7d84771e24e518eddba59d2cba
zahraa <zahraaalihasan279@gmail.com>
2024-03-13 02:06:44 +0300

    install CROS

--- GA-uni/server.js
+++ GA-uni/server.js
@@ -11,6 +11,7 @@ const coursesRouter = require('./routes/courses')
 const db = require('./db')
 
 var app = express()
+const cors = require('cors')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
@@ -21,6 +22,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(cors())
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
