d2cae082afa81209ed592a8fc92f18ab4979e26f
zahraa <zahraaalihasan279@gmail.com>
2024-03-12 19:31:08 +0300

    add course in server

--- GA-uni/server.js
+++ GA-uni/server.js
@@ -25,11 +25,13 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 app.use('/students', studentsRouter)
+
+app.use('/courses', coursesRouter)
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404))
 })
-app.use('/courses', coursesRouter)
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
